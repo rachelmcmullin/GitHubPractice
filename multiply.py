4ead118ae13f8e6af69f592d15c37099dd5522f7
elifober <eliasfobert@gmail.com>
2025-02-20 10:28:00 -0700

    Created a function to multiply two numbers
    Takes two numbers and multiplies them

---
+++ multiply.py
@@ -0,0 +1,9 @@
+#   Elias Fobert
+#   2/20/2025
+#   Take input and multiply it
+
+#   Takes two numbers and returns the product
+def multiplyNumberts(number1, number2) :
+    result = number1*number2
+    return result
+
